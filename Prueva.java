fa6303ea854b63c871115e6d77df1716def67d48
{Juan} <{elranchodejuancho12@gmail.com}>
2023-10-04 13:59:07 +0200

    modificación 1.1

--- Prueva.java
+++ Prueva.java
@@ -2,9 +2,9 @@ public class Prueva {
 
 	public static void main(String args[])	{
 
-		System.out.println("Blas Martínez");
 		System.out.println("18 años, 05/05/2005");
 		System.out.println("comida favorita: Espaguetti");
+		System.out.println("grupo favorito: Chubby mosh");
 
 	}
 
